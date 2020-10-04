c0685cb53626fe69e587fa4a1160acca76bafa43
LucaBert89 <62511269+LucaBert89@users.noreply.github.com>
2020-10-05 00:50:49 +0200

    service provided section added: desktop only

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,15 @@ const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
+    mode:"production",
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, "src", "index.html")
+      }),
+      new MiniCssExtractPlugin({
+          filename: "style.css"
+        })
+    ],
     module: {
         rules: [
           {
@@ -14,7 +23,7 @@ module.exports = {
               ],
           },
           { 
-            test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+            test: /\.(woff|woff2|eot|ttf)$/,
             exclude: /node_modules/,
             loader: 'file-loader',
             options: {
@@ -36,18 +45,25 @@ module.exports = {
             ]
           },
           {
+            test: /\.html$/i,
+            loader: 'html-loader',
+            options: {
+              attributes: {
+                list: [
+                  {
+                    tag: 'img',
+                    attribute: 'data-src',
+                    type: 'src',
+                  },
+                ]
+            }
+          }
+          },
+          {
             test: /\.js$/,
             exclude: /node_modules/,
             use: ["babel-loader"]
           }
         ]
       },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "src", "index.html")
-    }),
-    new MiniCssExtractPlugin({
-        filename: "style.css"
-      })
-  ]
 };
